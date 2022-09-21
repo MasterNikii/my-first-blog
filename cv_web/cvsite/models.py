390f3586504bac71e686f24c8387bff02fe61406
MasterNikii <nikitacoppola00@gmail.com>
2022-09-21 17:12:14 +0200

    commit sito personale da finire con il ciclo for

--- cv_web/cvsite/models.py
+++ cv_web/cvsite/models.py
@@ -1,16 +1,6 @@
 from django.db import models
 import datetime
 
-date = models.DateField(("Date"), default=datetime.date.today)
-
-# Create your models here.
-
-class cvelement(models.Model):
-    nome=models.CharField(max_length=20)
-    descrizione = models.CharField(max_length=60)
-    riassunto = models.CharField(max_length=500)
-    start = models.DateField()
-    end = models.DateField()
 
 class cvelement(models.Model):
     nome = models.CharField(max_length=100, default=None)
@@ -19,6 +9,5 @@ class cvelement(models.Model):
     start = models.DateField(("Date"), default=datetime.date.today)
     end = models.DateField(default=None, blank=True, null=True)
 
-
     def __str__(self):
-        return self.nome    
+        return self.nome
