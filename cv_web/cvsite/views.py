287dfaa8f942b2096fc9de0eb3e538965b585337
MasterNikii <nikitacoppola00@gmail.com>
2022-09-28 17:14:34 +0200

    css, aggiornamenti

--- cv_web/cvsite/views.py
+++ cv_web/cvsite/views.py
@@ -5,3 +5,5 @@ from .models import cvelement
 def cvsite(request):
         cvelements = cvelement.objects.all()
         return render(request, 'cvsite/homepage.html', {'cvelements': cvelements})
+def contatti(request):
+    return render(request, 'cvsite/contatti.html')
